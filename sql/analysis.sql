d748d4519ad7d45bbe3081c3065f1ac2ad711992
mgisselbeck <103837294+mattiegisselbeck@users.noreply.github.com>
2023-05-04 14:35:58 -0500

    Update analysis.sql

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,6 +1,7 @@
 -- Assessing Environmental Justice Concern in Twin Cities Metro Area
 
 /* Query 1
+Spatial Analysis
 Environmental Quality in Areas of Red Zones (holc_pred) and Concentrated Poverty (concpov) in Minneapolis */
 SELECT
     equity_considerations.tr10,
@@ -16,7 +17,38 @@ WHERE environmental_quality.environmental_quality_score > 26.5
 GROUP BY
     environmental_quality.environmental_quality_score, equity_considerations.ctu_prmry, equity_considerations_geom.geom_st, equity_considerations.env_pm25, equity_considerations.avg_temp, equity_considerations.pbipoc, equity_considerations.concpov, equity_considerations.tr10;
 
+
 /* Query 2
+Summary Statistics */
+-- Total Population (poptot_mc) of Census Tracts with and without Environmental Justice Concerns (tr_ej) 
+SELECT 
+  tr_ej, 
+  AVG(poptot_mc) AS avg_population, 
+  COUNT(*) AS count, 
+  MIN(poptot_mc) AS min_population, 
+  MAX(poptot_mc) AS max_population, 
+  ROUND(STDDEV_POP(poptot_mc), 2) AS std_dev_population 
+FROM 
+  equity_considerations
+GROUP BY 
+  tr_ej;
+
+-- Average Median Household Income (mdhhincnow) Census Tracts with and without Environmental Justice Concerns (tr_ej) 
+SELECT 
+  tr_ej, 
+  AVG(mdhhincnow) AS avg_mdhhincnow, 
+  COUNT(*) AS count, 
+  MIN(mdhhincnow) AS min_mdhhincnow, 
+  MAX(mdhhincnow) AS max_mdhhincnow, 
+  ROUND(CAST(STDDEV_POP(mdhhincnow) AS numeric), 2) AS std_dev_mdhhincnow
+FROM 
+  equity_considerations
+GROUP BY 
+  tr_ej;
+
+
+/* Query 3
+Environmental Quality Score
 Environmental Quality in Areas of Red Zones (holc_pred) and Concentrated Poverty (concpov) in Minneapolis and St. Paul */
 CREATE TABLE tcma_environmental_quality AS
 WITH scores AS (
@@ -25,7 +57,7 @@ WITH scores AS (
         tr10,
         tr_ej,
         holc_pred,
-		ROUND(((0.25 * (100 - avg_temp)) + (0.25 * (100 - env_cancer)) + (0.25 * (100 - (ghg_com_pe/10))) + (0.25 * (env_pm25/12)) + (0.25 * (100 - (env_dslpm/40))))) AS env_qs
+		ROUND(((0.20 * (100 - avg_temp)) + (0.20 * (100 - env_cancer)) + (0.20 * (100 - (ghg_com_pe/10))) + (0.20 * (env_pm25/12)) + (0.20 * (100 - (env_dslpm/40))))) AS env_qs
     FROM equity_considerations
     WHERE tr10 IS NOT NULL
     GROUP BY ctu_prmry, avg_temp, ghg_com_pe, env_cancer, env_pm25, concpov, tr10, env_dslpm, tr_ej, holc_pred, env_qs
@@ -34,7 +66,9 @@ WITH scores AS (
 SELECT *
 FROM scores;
 
-/* Query 3
+
+/* Query 4
+Environmental Justice Score (Prediction)
 Identifying Areas of Environmental Concern without the tr_ej Column */
 SELECT
     tr10,
@@ -64,18 +98,3 @@ FROM
 WHERE tr10 IS NOT NULL
 GROUP BY
     env_pm25, ctu_prmry, tr_ej, holc_pylw, tr_eda, holc_pred, avg_temp, phisppop, pbipoc, concpov, tr10;
-
-/*  Query 4 
-Linear Regression Model for Fine Particulate Matter PM 2.5 (env_pm2) and Total Population (poptot_mc) */
-SELECT
-    regr_slope(env_pm25, poptot_mc) AS slope,
-    regr_intercept(env_pm25, poptot_mc) AS intercept
-FROM
-    equity_considerations
-WHERE
-    tr_ej = 1
-    AND tr_eda = 1
-    AND pbipoc > 0.50
-    AND holc_pylw > 0.1
-    AND holc_pred > 0.1
-    AND concpov = 1;
--- sql/other.sql
+++ sql/other.sql
@@ -1,33 +1,86 @@
 -- Assessing Environmental Justice Concern in the Twin Cities Metro Area
 
+-- Summary Statistics --
 ------------------------------------
--- Average Population of Census Tracts with and without Environmental Justice Concerns (tr_ej) 
-SELECT tr_ej, AVG(poptot_mc) AS avg_population
-FROM environmental_justice
-GROUP BY tr_ej;
-
+-- Total Population (poptot_mc) of Census Tracts with and without Environmental Justice Concerns (tr_ej) 
+SELECT 
+  tr_ej, 
+  AVG(poptot_mc) AS avg_population, 
+  COUNT(*) AS count, 
+  MIN(poptot_mc) AS min_population, 
+  MAX(poptot_mc) AS max_population, 
+  ROUND(STDDEV_POP(poptot_mc), 2) AS std_dev_population 
+FROM 
+  equity_considerations
+GROUP BY 
+  tr_ej;
 ------------------------------------
 -- Average Median Household Income (mdhhincnow) Census Tracts with and without Environmental Justice Concerns (tr_ej) 
-SELECT tr_ej, AVG(mdhhincnow) AS avg_income
-FROM equity_considerations
-GROUP BY tr_ej;
+SELECT 
+  tr_ej, 
+  AVG(mdhhincnow) AS avg_mdhhincnow, 
+  COUNT(*) AS count, 
+  MIN(mdhhincnow) AS min_mdhhincnow, 
+  MAX(mdhhincnow) AS max_mdhhincnow, 
+  ROUND(CAST(STDDEV_POP(mdhhincnow) AS numeric), 2) AS std_dev_mdhhincnow
+FROM 
+  equity_considerations
+GROUP BY 
+  tr_ej;
 
 ------------------------------------
--- Total Population (poptot_mc) for Census Tracts with Concentrated Poverty (concpov) and a High Proportion of BIPOC (pbipoc)
-SELECT ctu_prmry, SUM(poptot_mc) AS total_population
-FROM equity_considerations
-WHERE concpov = 1 AND pbipoc > 0.1
-GROUP BY ctu_prmry
+-- Total Population (poptot_mc) for Census Tracts with Concentrated Poverty (concpov) and a High Proportion of BIPOC (pbipoc) with and without Environmental Justice Concerns (tr_ej) 
+SELECT 
+  tr_ej, 
+  AVG(poptot_mc) AS poptot_mc, 
+  COUNT(*) AS count, 
+  MIN(poptot_mc) AS min_poptot_mc, 
+  MAX(poptot_mc) AS max_poptot_mc, 
+  ROUND(CAST(STDDEV_POP(poptot_mc) AS numeric), 2) AS std_dev_poptot_mc
+FROM 
+  equity_considerations
+WHERE concpov = 1 AND pbipoc > 0.5
+GROUP BY 
+  tr_ej;
 
 ------------------------------------
--- Average Proportion of BIPOC (pbipoc), and Hispanic or Latinx (phisppop) in Areas of Environmental Justice Concern (tr_ej)
-SELECT tr_ej, AVG(pbipoc) AS avg_pbipoc, AVG(phisppop) AS avg_phisppop
-FROM environmental_justice
-GROUP BY tr_ej;
+-- Proportion of BIPOC (pbipoc) and Hispanic (phisppop) Population with and without Environmental Justice Concerns (tr_ej)
+SELECT 
+  tr_ej, 
+  AVG(pbipoc + phisppop) AS minpop, 
+  COUNT(*) AS count, 
+  MIN(pbipoc + phisppop) AS min_minpop, 
+  MAX(pbipoc + phisppop) AS max_minpop, 
+  ROUND(CAST(STDDEV_POP(pbipoc + phisppop) AS numeric), 2) AS std_dev_minpop
+FROM 
+  equity_considerations
+GROUP BY 
+  tr_ej;
 
 ------------------------------------
--- Average Temperature by Areas of Environmental Justice Concern (tr_ej)
-SELECT tr_ej, AVG(avg_temp) AS avg_temp
-FROM environmental_justice
-GROUP BY tr_ej;
+-- Average Land Surface Temperature (avg_temp) with and without Environmental Justice Concerns (tr_ej)
+SELECT 
+  tr_ej, 
+  AVG(avg_temp) AS avg_tmp,
+  COUNT(*) AS count, 
+  MIN(avg_temp) AS min_avg_temp, 
+  MAX(avg_temp) AS max_avg_temp, 
+  ROUND(CAST(STDDEV_POP(avg_temp) AS numeric), 2) AS std_dev_avg_temp
+FROM 
+  equity_considerations
+GROUP BY 
+  tr_ej;
 
+------------------------------------
+-- Concentrated Poverty (concpov) with and without Environmental Justice Concerns (tr_ej)
+SELECT 
+  tr_ej, 
+  AVG(concpov) AS avg_concpov,
+  COUNT(*) AS count, 
+  MIN(concpov) AS min_concpov, 
+  MAX(concpov) AS max_concpov, 
+  ROUND(CAST(STDDEV_POP(concpov) AS numeric), 2) AS std_dev_concpov
+FROM 
+  equity_considerations
+GROUP BY 
+  tr_ej;
